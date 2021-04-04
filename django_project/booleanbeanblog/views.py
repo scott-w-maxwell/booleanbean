5a118591072dac9b8880e71f124795c4e556591e
Scott Maxwell <scottwillardmaxwell@protonmail.com>
2021-04-03 21:40:04 -0700

    added posts functionality

--- django_project/booleanbeanblog/views.py
+++ django_project/booleanbeanblog/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+# Dummy data
+posts = [
+    {
+        'author': 'Scott M',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '4-3-2021'
+    },
+    {
+        'author': 'Scott M',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': '4-3-2021'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'home.html', context)
