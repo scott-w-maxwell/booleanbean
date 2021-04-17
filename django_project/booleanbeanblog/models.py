45a8cb67459e8081b7107604776fae74051d7cb9
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2021-04-17 13:18:19 -0700

    added posts model and connected to home page

--- django_project/booleanbeanblog/models.py
+++ django_project/booleanbeanblog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+
+	title = models.TextField(blank=False)
+	author = models.TextField(blank=False)
+	content = models.TextField(blank=False)
+	date_posted = models.TimeField(blank=False)
+
--- django_project/booleanbeanblog/views.py
+++ django_project/booleanbeanblog/views.py
@@ -2,21 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Dummy data
-posts = [
-    {
-        'author': 'Scott M',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '4-3-2021'
-    },
-    {
-        'author': 'Scott M',
-        'title': 'Blog Post 2',
-        'content': 'First post content',
-        'date_posted': '4-3-2021'
-    }
-]
+from .models import Post
+
+posts = Post.objects.all()
 
 
 def home(request):
