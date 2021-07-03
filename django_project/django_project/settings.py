dab3fca2cdecaf1f4017bbb68ca71f166656d699
scott-w-maxwell <scottwillardmaxwell@protonmail.com>
2021-07-03 09:15:07 -0700

    changed security

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bx9wn0a!u80g#x@vgj#a0q3yb+igl99oh)i#$v7x#jj0p4sspr'
-
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
